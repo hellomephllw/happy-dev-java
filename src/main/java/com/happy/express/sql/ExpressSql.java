b6848b6edbaa65a9f45ff6db0b1c5aa782dcd554
hellomephllw <hellomephllw@163.com>
2019-05-09 23:02:48 +0800

    引入express, 加载所有实体

---
+++ src/main/java/com/happy/express/sql/ExpressSql.java
@@ -0,0 +1,103 @@
+package com.happy.express.sql;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description: 引入迅捷Sql
+ * @author: llw
+ * @date: 2019-05-09
+ */
+public class ExpressSql {
+
+    /**logger*/
+    private static Logger logger = LoggerFactory.getLogger(ExpressSql.class);
+
+    /**所有实体, 键为包路径, 值为实体类模板*/
+    public static Map<String, Class> entities = new HashMap<>();
+    /**实体包路径*/
+    private static String entitiesPackagePath = null;
+
+    /**
+     * 开启
+     * @param entitiesPackagePath 实体包路径
+     * @param openObverse 正向sql(对象to sql)
+     * @param openReverse 逆向sql(直接sql)
+     */
+    public static ExpressSql startup(String entitiesPackagePath, boolean openObverse, boolean openReverse) {
+        try {
+            //初始化实体包路径
+            ExpressSql.entitiesPackagePath = entitiesPackagePath;
+            //读取所有实体
+            entitiesReader();
+        } catch (Exception e) {
+            logger.error("启动ExpressSql发生错误", e);
+        }
+
+        return new ExpressSql();
+    }
+
+    /**
+     * 获取实体
+     * @return 实体map集合
+     */
+    public static Map<String, Class> getEntities() {
+        return entities;
+    }
+
+    /**
+     * 读取所有实体
+     */
+    private static void entitiesReader() throws Exception {
+        File packageDir = new File(getRootClassPath() + transferDotToSlash(entitiesPackagePath));
+        for (File file : packageDir.listFiles()) {
+            if (file.isFile()) {
+                //基本模块
+                collectClasses(file, null);
+            } else {
+                //子模块
+                String dirName = file.getName();
+                for (File innerFile : file.listFiles()) {
+                    collectClasses(innerFile, dirName);
+                }
+            }
+        }
+    }
+
+    /**
+     * 获取类加载路径根目录
+     * @return 类加载路径根目录
+     */
+    private static String getRootClassPath() {
+        return ExpressSql.class.getResource("/").getPath();
+    }
+
+    /**
+     * 把点变为斜杠
+     * @param path 路径
+     * @return 转换后的路径
+     */
+    private static String transferDotToSlash(String path) {
+        return path.replaceAll("\\.", "/");
+    }
+
+    /**
+     * 收集类模板
+     * @param file 文件
+     * @param childModuleName 子模块
+     * @throws Exception
+     */
+    private static void collectClasses(File file, String childModuleName) throws Exception {
+        if (file.isFile() && file.getName().endsWith(".class")) {
+            String fileName = file.getName().split("\\.")[0];
+            String classPath = entitiesPackagePath + "." + (childModuleName != null ? childModuleName + "." : "") + fileName;
+            Class entityClass = ExpressSql.class.getClassLoader().loadClass(classPath);
+            entities.put(classPath, entityClass);
+        }
+    }
+
+}
