9829b9a06ea82f25ab4f8379ba4c0466cc579709
hellomephllw <hellomephllw@163.com>
2019-08-16 14:39:59 +0800

    timestamp取消默认值

--- src/main/java/com/happy/express/persist/mysql/helper/DatabaseHelper.java
+++ src/main/java/com/happy/express/persist/mysql/helper/DatabaseHelper.java
@@ -650,7 +650,7 @@ public class DatabaseHelper {
                 }
                 return "varchar";
             } else if (fieldType == Date.class || fieldType == java.util.Date.class || fieldType == Timestamp.class) {
-                return "timestamp default 0";
+                return "timestamp";
             } else if (fieldType == BigDecimal.class) {
                 return "decimal";
             } else if (fieldType == Byte.class) {
