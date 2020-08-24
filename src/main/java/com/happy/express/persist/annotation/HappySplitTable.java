7cd93ebb37ec471db5f48a6911944e9d0e0dbea3
hellomephllw <hellomephllw@163.com>
2020-08-24 14:56:44 +0800

    取消entity注解上的分表属性，加入dao方法的分表注解

--- src/main/java/com/happy/express/persist/annotation/HappySplitTable.java
+++ src/main/java/com/happy/express/persist/annotation/HappySplitTable.java
@@ -5,10 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE)
+@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface HappyTable {
+public @interface HappySplitTable {
 
     public String tableName() default "";
 
+    public Class splitTableStrategy();
 }
